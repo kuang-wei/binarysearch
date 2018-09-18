c0657a14a6c35f6d7c05c593220c89e2c09d13c6
kuang-wei <kuangwei@uchicago.edu>
2018-09-18 11:33:18 -0500

    add docstring for binarysearch

---
+++ binarysearch.py
@@ -0,0 +1,16 @@
+def binarysearch(inputlist, target):
+	"""
+	binary search for a sorted list
+
+	Input:
+		inputlist: list
+			the sorted list to be searched through
+
+		target: int, float
+			the target item to search for
+	
+	Return:
+		target_idx: int
+			the index of the target
+	"""
+	return None
