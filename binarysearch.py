f53e349f042711dc2fd49a7163ef6abccad148f0
kuang-wei <kuangwei@uchicago.edu>
2018-09-18 11:56:42 -0500

    implement recursive algorithm

--- binarysearch.py
+++ binarysearch.py
@@ -1,16 +1,27 @@
 def binarysearch(inputlist, target):
 	"""
-	binary search for a sorted list
+	binary search for a target in a sorted list
+	function returns True if target is in the list, False otherwise
 
 	Input:
 		inputlist: list
 			the sorted list to be searched through
-
 		target: int, float
 			the target item to search for
 	
 	Return:
-		target_idx: int
-			the index of the target
+		True or False
 	"""
-	return None
+	listlen = len(inputlist)
+	if listlen==0:					# if list is empty, search is done
+		return False
+
+	mididx = (len(inputlist))//2	# index of the middle item
+	midval = inputlist[mididx]		# value of the middle item
+	if midval==target:
+		return True
+	else:
+		if midval>target:
+			return binarysearch(inputlist[:mididx], target)
+		else:
+			return binarysearch(inputlist[mididx+1:], target)
